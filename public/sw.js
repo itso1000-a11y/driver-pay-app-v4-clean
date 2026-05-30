afd08d5651741b8ec53480de37188cb85bb4070b
itso1000-a11y <itso1000@gmail.com>
2026-05-30 19:45:36 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "driver-pay-v4-cache-v4-28";
+const CACHE_NAME = "driver-pay-v4-cache-v4-30";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
